e64aa03eee65bde2da56e16417500d27873116a4
James Araujo <jaraujo@sfu.ca>
2017-03-07 17:30:18 -0800

    Further improved homepage interactivity

--- web-app/index.js
+++ web-app/index.js
@@ -27,3 +27,15 @@ ToC +=
   "</nav>";
 
 $(".all-questions").prepend(ToC);
+
+$("h3, h3 a").hover(function(){ // hover on table of contents links
+  $(this).addClass("liHovered");
+}, function(){
+  $(this).removeClass("liHovered"); // when not hovered
+});
+
+$(".all-questions li a").hover(function(){ // hover on table of contents links
+  $(this).addClass("liHovered2");
+}, function(){
+  $(this).removeClass("liHovered2"); // when not hovered
+});
